8cc4115c00b7b45d49c3593ad68194dbe97bc378
Andrey Roditelev <rodandr13@gmail.com>
2024-03-20 22:49:45 +0300

    feat: add contacts block

--- src/shared/ui/navbar/Navbar.tsx
+++ src/shared/ui/navbar/Navbar.tsx
@@ -20,11 +20,31 @@ export const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <div>Logo</div>
-      {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
-      ))}
+      <div>
+        {links.map((link) => (
+          <Link href={link.path} key={link.title}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+      <div>
+        <span>English</span>
+        <span>USD</span>
+      </div>
+      <div className={styles.contacts}>
+        <div className={styles.contacts__telephone}>
+          <span className={styles.contacts__telephoneCode}>+429</span> 725 841
+          086
+        </div>
+        <ul className={styles.contacts__social}>
+          <li
+            className={`${styles.contacts__socialItem} ${styles.contacts__telegram}`}
+          >
+            Telegram
+          </li>
+          <li className={`${styles.contacts__socialItem} ${styles.contacts__whatsapp}`}>WhatsApp</li>
+        </ul>
+      </div>
     </div>
   );
 };
