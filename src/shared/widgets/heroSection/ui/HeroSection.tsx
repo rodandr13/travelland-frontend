40878d21879a693c51799a48f51e0ab44b7c0618
Andrey Roditelev <rodandr13@gmail.com>
2024-03-23 21:48:49 +0300

    feat: edit hero block

--- src/shared/widgets/heroSection/ui/HeroSection.tsx
+++ src/shared/widgets/heroSection/ui/HeroSection.tsx
@@ -4,7 +4,11 @@ import { ReactNode } from "react";
 export const HeroSection = ({ children }: { children: ReactNode }) => {
   return (
     <section className={styles.hero}>
-      <div className={styles.hero__container}>{children}</div>
+      {children}
+      <div className={styles.hero__container}>
+        <h1 className={styles.hero__title}>Book unique places for rest and entertainment</h1>
+        <button className={styles.hero__button}>VIEW EXCURSIONS</button>
+      </div>
     </section>
   );
 };
