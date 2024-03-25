69fd9d72719634a7a4aaa3dbeffd833b20d7c480
Andrey Roditelev <rodandr13@gmail.com>
2024-03-25 21:35:06 +0300

    feat: add addFavorite component

--- src/features/excursion/excursionFilter/ui/filtertList/FilterList.tsx
+++ src/features/excursion/excursionFilter/ui/filtertList/FilterList.tsx
@@ -1,11 +1,16 @@
 import styles from "./styles.module.scss";
 import { FilterItem } from "../filterItem";
+import clsx from "clsx";
 
 const filters = [1, 2, 3, 4, 5, 6];
 
-export const FilterList = () => {
+interface Props {
+  className: string;
+}
+
+export const FilterList = ({ className }: Props) => {
   return (
-    <section className={styles.filterList}>
+    <section className={clsx(styles.filterList, className)}>
       <form className={styles.filterList__form}>
         {filters.map((filter) => (
           <FilterItem key={filter} />
