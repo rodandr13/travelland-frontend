af6a204f71ea30458ffee09e0d5c0e0f485a0cb9
Andrey Roditelev <rodandr13@gmail.com>
2024-04-01 02:19:09 +0300

    feat: fixed scroll when menu open

--- src/widgets/header/ui/navbar/ui/HamburgerButton.tsx
+++ src/widgets/header/ui/navbar/ui/HamburgerButton.tsx
@@ -1,12 +1,52 @@
+"use client";
+
 import clsx from "clsx";
 import styles from "./styles.module.scss";
 import { useAppDispatch, useAppSelector } from "@/src/shared/lib/redux/hooks";
 import { toggleMenu } from "@/src/widgets/header/model/menuSlice";
+import sharedStyles from "@/src/shared/styles/styles.module.scss";
+import { useEffect } from "react";
 
 export const HamburgerButton = () => {
   const dispatch = useAppDispatch();
   const isOpen = useAppSelector((state) => state.menu.isOpen);
 
+  const handleResize = () => {
+    if (window.innerWidth >= 750 && isOpen) {
+      console.log("test");
+      dispatch(toggleMenu());
+      document.body.classList.remove(sharedStyles.disableScroll);
+      const pagePosition = parseInt(document.body.dataset.position || "0", 10);
+      window.scrollTo(0, pagePosition);
+      document.body.style.top = "";
+      document.body.removeAttribute("data-position");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [isOpen]);
+
+  useEffect(() => {
+    const body = document.body;
+    if (isOpen) {
+      const pagePosition = window.scrollY;
+      body.dataset.position = pagePosition.toString();
+      body.style.top = -pagePosition + "px";
+      body.classList.add(sharedStyles.disableScroll);
+    } else {
+      body.style.top = "";
+      body.classList.remove(sharedStyles.disableScroll);
+      const pagePosition = parseInt(body.dataset.position || "0", 10);
+      window.scrollTo(0, pagePosition);
+      body.removeAttribute("data-position");
+    }
+  }, [isOpen]);
+
+  const handleClick = () => {
+    dispatch(toggleMenu());
+  };
   return (
     <button
       className={clsx(
@@ -15,7 +55,7 @@ export const HamburgerButton = () => {
       )}
       aria-label="Меню"
       type="button"
-      onClick={() => dispatch(toggleMenu())}
+      onClick={handleClick}
     >
       <span className={`${styles.hamburger__buttonLine}`} />
       <span className={`${styles.hamburger__buttonLine}`} />
