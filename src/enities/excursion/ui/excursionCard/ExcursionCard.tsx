f4504341a09cf76626911f6114db51a68b110b58
Andrey Roditelev <rodandr13@gmail.com>
2024-04-03 04:35:19 +0300

    feat: add slider component

--- src/enities/excursion/ui/excursionCard/ExcursionCard.tsx
+++ src/enities/excursion/ui/excursionCard/ExcursionCard.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import styles from "./styles.module.scss";
-import Image from "next/image";
-import clsx from "clsx";
-import { NavigateBefore, NavigateNext } from "@mui/icons-material";
 import { WeekDays } from "@/src/shared/ui/weekDays";
 import { PriceBlock } from "@/src/shared/ui/priceBlock";
 import Link from "next/link";
 import React from "react";
-import { urlFor } from "@/src/shared/lib/sanity/client";
+import { ImageSlider } from "@/src/shared/ui/slider";
 
 interface Props {
   addFavorite: React.ReactNode;
@@ -26,10 +23,6 @@ const daysOfWeek = [
 ];
 
 export const ExcursionCard = ({ addFavorite, card }: Props) => {
-  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-  };
-  const imageUrl = urlFor(card.gallery[1]);
   const price = card.schedule[1].prices[0].price;
   const duration = card.schedule[1].duration.hours;
   const weekdays = card.schedule[1].weekdays;
@@ -40,47 +33,8 @@ export const ExcursionCard = ({ addFavorite, card }: Props) => {
     <article className={styles.excursionCard}>
       <Link href="/excursion" className={styles.excursionCard__link}>
         <div className={styles.excursionCard__imageContainer}>
-          <Image
-            className={styles.excursionCard__image}
-            src={imageUrl}
-            fill={true}
-            alt="ExcursionCard image"
-          />
           <div className={styles.excursionCard__favorites}>{addFavorite}</div>
-          <ul
-            className={clsx(
-              styles.sliderControls,
-              styles.excursionCard__imageIncidator
-            )}
-          >
-            <li className={styles.sliderControls__item}>
-              <button className={styles.sliderControls__button} />
-            </li>
-            <li className={styles.sliderControls__item}>
-              <button className={styles.sliderControls__button} />
-            </li>
-            <li className={styles.sliderControls__item}>
-              <button className={styles.sliderControls__button} />
-            </li>
-            <li className={styles.sliderControls__item}>
-              <button className={styles.sliderControls__button} />
-            </li>
-            <li className={styles.sliderControls__item}>
-              <button className={styles.sliderControls__button} />
-            </li>
-          </ul>
-          <button
-            onClick={handleClick}
-            className={clsx(styles.slideButton, styles.slideButton_prev)}
-          >
-            <NavigateBefore />
-          </button>
-          <button
-            onClick={handleClick}
-            className={clsx(styles.slideButton, styles.slideButton_next)}
-          >
-            <NavigateNext />
-          </button>
+          <ImageSlider images={card.gallery} />
         </div>
         <WeekDays days={filledDays} />
         <h3 className={styles.excursionCard__header}>{card.title}</h3>
---
+++ src/shared/ui/slider/ui/ImageSlider.tsx
@@ -0,0 +1,98 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+import clsx from "clsx";
+import { NavigateBefore, NavigateNext } from "@mui/icons-material";
+import React, { useRef, useState } from "react";
+import { urlFor } from "@/src/shared/lib/sanity/client";
+import scrollIntoView from "scroll-into-view-if-needed";
+
+interface Props {
+  images: string[];
+}
+
+export const ImageSlider = ({ images }: Props) => {
+  const imageUrl = urlFor(images[1]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slideRefs = useRef<HTMLElement[]>([]);
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    console.log("lick");
+  };
+  const goToSlide = (index: number) => {
+    if (index < 0 || index >= images.length) return;
+    setCurrentIndex(index);
+    scrollIntoView(slideRefs.current[index], {
+      scrollMode: "if-needed",
+      block: "nearest",
+      inline: "nearest",
+      behavior: "smooth",
+    });
+  };
+
+  const setSlideRef = (ref: HTMLLIElement | null, index: number) => {
+    if (ref) slideRefs.current[index] = ref;
+  };
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.imageSlider}>
+        <ul className={styles.imageSlider__list}>
+          {images.map((image, i) => (
+            <li
+              key={i}
+              className={styles.imageSlider__item}
+              ref={(ref) => setSlideRef(ref, i)}
+            >
+              <Image
+                className={styles.imageSlider__image}
+                src={urlFor(image)}
+                fill
+                alt="ExcursionCard image"
+              />
+            </li>
+          ))}
+        </ul>
+        <ul
+          className={clsx(
+            styles.sliderControls,
+            styles.imageSlider__imageIncidator
+          )}
+        >
+          <li className={styles.sliderControls__item}>
+            <button className={styles.sliderControls__button} />
+          </li>
+          <li className={styles.sliderControls__item}>
+            <button className={styles.sliderControls__button} />
+          </li>
+          <li className={styles.sliderControls__item}>
+            <button className={styles.sliderControls__button} />
+          </li>
+          <li className={styles.sliderControls__item}>
+            <button className={styles.sliderControls__button} />
+          </li>
+          <li className={styles.sliderControls__item}>
+            <button className={styles.sliderControls__button} />
+          </li>
+        </ul>
+      </section>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          goToSlide(currentIndex - 1);
+        }}
+        className={clsx(styles.slideButton, styles.slideButton_prev)}
+      >
+        <NavigateBefore />
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          goToSlide(currentIndex + 1);
+        }}
+        className={clsx(styles.slideButton, styles.slideButton_next)}
+      >
+        <NavigateNext />
+      </button>
+    </div>
+  );
+};
