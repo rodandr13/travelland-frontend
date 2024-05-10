0c463fab806f3d8a603f7c5c4be308e3d9a4507d
Andrey Roditelev <rodandr13@gmail.com>
2024-05-10 03:34:16 +0300

    feat: google maps visible styles

--- src/shared/ui/map/Map.tsx
+++ src/shared/ui/map/Map.tsx
@@ -9,31 +9,40 @@ interface Props {
   lat: number;
 }
 
+let googleMapsLoaded = false;
+
 export const Map = ({ lng, lat }: Props) => {
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const initMap = async () => {
-      const loader = new Loader({
-        apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-        version: "weekly",
-      });
+      if (!googleMapsLoaded) {
+        const loader = new Loader({
+          apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+          version: "weekly",
+        });
 
-      const { AdvancedMarkerElement } = await loader.importLibrary("marker");
+        const { AdvancedMarkerElement } = await loader.importLibrary("marker");
 
-      const { Map } = await loader.importLibrary("maps");
-      const map = new Map(ref.current as HTMLDivElement, {
-        center: { lng, lat },
-        zoom: 15,
-        mapId: "map",
-      });
-      const marker = new AdvancedMarkerElement({
-        map: map,
-        position: { lng, lat },
-      });
+        const { Map } = await loader.importLibrary("maps");
+
+        const map = new Map(ref.current as HTMLDivElement, {
+          center: { lng, lat },
+          zoom: 15,
+          mapId: "map",
+          rotateControl: false,
+          streetViewControl: false,
+          mapTypeControl: false,
+        });
+        const marker = new AdvancedMarkerElement({
+          map: map,
+          position: { lng, lat },
+        });
+        googleMapsLoaded = true;
+      }
     };
 
     initMap();
-  }, [ref]);
+  }, [lng, lat]);
 
   return <div ref={ref} className={styles.map}></div>;
 };
