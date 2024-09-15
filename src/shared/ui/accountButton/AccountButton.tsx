00e76f154854247f359a3c85e19711519a032799
Andrey Roditelev <rodandr13@gmail.com>
2024-09-15 23:25:42 +0300

    feat: add account button

---
+++ src/shared/ui/accountButton/AccountButton.tsx
@@ -0,0 +1,75 @@
+import { Avatar, Group, Menu, Text, UnstyledButton } from "@mantine/core";
+import {
+  IconListDetails,
+  IconLogout2,
+  IconUserScan,
+} from "@tabler/icons-react";
+
+import { useSignOut } from "@/src/features/signOut";
+
+import styles from "./styles.module.scss";
+
+export const AccountButton = () => {
+  const signOut = useSignOut();
+
+  const handleSignOut = () => {
+    signOut();
+  };
+
+  return (
+    <Menu
+      shadow="md"
+      width={200}
+      trigger="click-hover"
+      openDelay={100}
+      closeDelay={400}
+      position="bottom-end"
+    >
+      <Menu.Target>
+        <UnstyledButton className={styles.accountButton}>
+          <Group gap="0">
+            <Avatar
+              src={null}
+              alt=""
+              variant="transparent"
+              color="rgba(77, 77, 77, 1)"
+            />
+            <div style={{ flex: 1 }}>
+              <Text size="sm" fw={500}>
+                Harriette Spoonlicker
+              </Text>
+
+              <Text c="dimmed" size="xs">
+                hspoonlicker@outlook.com
+              </Text>
+            </div>
+          </Group>
+        </UnstyledButton>
+      </Menu.Target>
+      <Menu.Dropdown>
+        <Menu.Item
+          leftSection={
+            <IconUserScan style={{ width: "18px", height: "18px" }} />
+          }
+        >
+          Профиль
+        </Menu.Item>
+        <Menu.Item
+          leftSection={
+            <IconListDetails style={{ width: "18px", height: "18px" }} />
+          }
+        >
+          Бронирования
+        </Menu.Item>
+        <Menu.Item
+          leftSection={
+            <IconLogout2 style={{ width: "18px", height: "18px" }} />
+          }
+          onClick={handleSignOut}
+        >
+          Выйти
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
+};
